cdaac1f4eb85c22142da5d5d04131ee4bd62fb38
Crowdbotics <team@crowdbotics.com>
2023-08-30 19:58:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-morning-43154",
+  url: "https://tiny-morning-43154.botics.co",
+  api: "https://tiny-morning-43154.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
